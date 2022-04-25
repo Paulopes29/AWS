04b69cc947be5db2ab7aa554a62430367e1c9ba9
Betamil <hugoribeyron1@gmail.com>
2022-04-25 16:17:50 +0200

    feat: Création du composant Calculatrice

---
+++ src/Components/Calculatrice/Calculatrice.js
@@ -0,0 +1,11 @@
+import style from './Calculatrice.module.css'
+
+function Calculatrice() {
+    return(
+        <div className={style.divCenter}>
+            <div className={style.background}>bonjour</div>
+        </div>
+    )
+}
+
+export default Calculatrice;

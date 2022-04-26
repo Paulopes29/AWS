c71e22f647cf8f81ff6cc3f2a76f9a764d62388d
Paulopes29 <paulo29lopesalves@gmail.com>
2022-04-26 10:22:55 +0200

    tests Lopes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Button from "./Components/Button/Button.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button text={"tg"}></Button>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {sum} from "./Components/Calcul/Calcul";
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(sum([1,2,3])).toBe(6);
 });
+
+
